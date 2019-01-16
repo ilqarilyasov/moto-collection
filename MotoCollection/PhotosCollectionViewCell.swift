0c600c4729e287b0e87f6260faeab1bd79bb1750
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-01-16 09:18:33 -0600

    Set storyboard, create subclasses and make outlet connections

---
+++ MotoCollection/PhotosCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosCollectionViewCell.swift
+//  MotoCollection
+//
+//  Created by Ilgar Ilyasov on 1/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoLabel: UILabel!
+}
