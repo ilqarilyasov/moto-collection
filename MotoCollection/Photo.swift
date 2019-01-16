0e5044087e951a01df421acad48f9e5255099bf3
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-01-16 08:35:15 -0600

    Add Photo model struct

---
+++ MotoCollection/Photo.swift
@@ -0,0 +1,15 @@
+//
+//  Photo.swift
+//  MotoCollection
+//
+//  Created by Ilgar Ilyasov on 1/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    
+    var imageData: Data
+    var title: String
+}
