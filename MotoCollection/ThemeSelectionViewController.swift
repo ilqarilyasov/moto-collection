5e7b7c6461c5b26d9b30b11d5f81efea17433289
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-01-16 09:39:58 -0600

    Lay out ThemeSelectionView. Create a subclass for it, then make IBAction connections

---
+++ MotoCollection/ThemeSelectionViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ThemeSelectionViewController.swift
+//  MotoCollection
+//
+//  Created by Ilgar Ilyasov on 1/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+    
+    var themeHelper: ThemeHelper?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func setDarkTheme(_ sender: Any) {
+        
+    }
+    
+    @IBAction func setLightTheme(_ sender: Any) {
+    }
+}
